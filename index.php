b92086a1e582b8571ccf43795fad5f6a7043463f
tai <tai@wicrew.com>
2016-02-09 16:36:47 +0700

    Counter 01

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$hit_count = @file_get_contents('count.txt'); // read the hit count from file
+echo $hit_count; //  display the hit count
+$hit_count++; // increment the hit count by 1
+@file_put_contents('count.txt', $hit_count); // store the new hit count
+
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	Test Counter
+</body>
+</html>
